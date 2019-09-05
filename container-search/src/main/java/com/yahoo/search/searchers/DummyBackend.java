05062c8bb8b84d018fdde2348685d895eda8115e
Henning Baldersheim <balder@yahoo-inc.com>
2019-09-05 16:27:48 +0200

    D onot provide backend

--- container-search/src/main/java/com/yahoo/search/searchers/DummyBackend.java
+++ container-search/src/main/java/com/yahoo/search/searchers/DummyBackend.java
@@ -4,14 +4,11 @@ import com.yahoo.search.Query;
 import com.yahoo.search.Result;
 import com.yahoo.search.Searcher;
 import com.yahoo.search.searchchain.Execution;
-import com.yahoo.component.chain.dependencies.Provides;
 import com.yahoo.component.chain.dependencies.After;
-import com.yahoo.search.searchchain.PhaseNames;
 
 /**
  * @author baldersheim
  */
-@Provides(PhaseNames.BACKEND)
 @After("*")
 public class DummyBackend extends Searcher {
 
